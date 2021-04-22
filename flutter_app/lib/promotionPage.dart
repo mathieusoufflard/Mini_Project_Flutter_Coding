7ba368fd3096d7ef70b66c89b59a5049368fc2f5
Yanis Verbeke <yanis88357@gmail.com>
2021-04-22 11:33:13 +0200

    Add input helper

--- flutter_app/lib/promotionPage.dart
+++ flutter_app/lib/promotionPage.dart
@@ -3,31 +3,12 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/Controllers/promotionController.dart';
+import 'package:flutter_app/Helpers/inputHelper.dart';
 
 class PromotionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = 'Promotion';
-
-    _setInputField(String hintText, TextEditingController fieldController,
-        TextInputType inputType,
-        [bool readOnly = false]) {
-      return Container(
-        width: MediaQuery.of(context).size.width / 5,
-        margin: EdgeInsets.all(10),
-        child: TextField(
-          controller: fieldController,
-          keyboardType: inputType,
-          inputFormatters: inputType == TextInputType.number
-              ? <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp('[0-9.]'))
-                ]
-              : [],
-          decoration: InputDecoration(hintText: hintText),
-          readOnly: readOnly,
-        ),
-      );
-    }
 
     PromotionController promotionController = new PromotionController();
 
@@ -39,17 +20,25 @@ class PromotionPage extends StatelessWidget {
         child: Column(
           children: [
             Text("Prix initial : "),
-            _setInputField("0", promotionController.initPriceController,
-                TextInputType.number),
+            InputHelper().setInputField(context, "0",
+                promotionController.initPriceController, TextInputType.number),
             Text("Pourcentage de réduction : "),
-            _setInputField("0", promotionController.promoRateController,
-                TextInputType.number),
+            InputHelper().setInputField(context, "0",
+                promotionController.promoRateController, TextInputType.number),
             Text("Valeur de réduction : "),
-            _setInputField("0", promotionController.promoValueController,
-                TextInputType.number, true),
+            InputHelper().setInputField(
+                context,
+                "0",
+                promotionController.promoValueController,
+                TextInputType.number,
+                true),
             Text("Prix final : "),
-            _setInputField("0", promotionController.finalPriceController,
-                TextInputType.number, true),
+            InputHelper().setInputField(
+                context,
+                "0",
+                promotionController.finalPriceController,
+                TextInputType.number,
+                true),
             ElevatedButton(
                 onPressed: () {
                   if (promotionController.initPriceController.text.isEmpty) {
