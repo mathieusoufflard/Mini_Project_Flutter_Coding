e8e776d91d0ccd99f2babe501fba493a3e206bd9
Yanis Verbeke <yanis88357@gmail.com>
2021-04-23 12:26:04 +0200

    Fix distance page

--- flutter_app/lib/distancePage.dart
+++ flutter_app/lib/distancePage.dart
@@ -1,25 +1,30 @@
 import 'package:flutter_app/Helpers/inputHelper.dart';
 import 'package:units_converter/Length.dart';
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:romanice/romanice.dart';
 
-String dropdownValue = 'mètre';
-String dropdownValueBottom = 'mètre';
-String valueTop = "";
+class DistancePage extends StatefulWidget {
+  const DistancePage({Key? key}) : super(key: key);
 
+  @override
+  _DistancePageState createState() => _DistancePageState();
+}
 
-class DistancePage extends StatelessWidget {
+class _DistancePageState extends State<DistancePage> {
+  TextEditingController firstInputController = TextEditingController();
+  TextEditingController secondInputController = TextEditingController();
+  String dropdownValue = 'mètre';
+  String dropdownValueBottom = 'mètre';
+  String valueTop = "";
 
-  TextEditingController decimalData = TextEditingController();
-  TextEditingController romanData = TextEditingController();
-
-  double? selectMesure(value,mesure,type) {
+  double? selectMesure(value, mesure, type) {
     value = double?.parse(value);
-    var length = Length(removeTrailingZeros: false); //initialize Length object, let's specify that we want to keep the trailing zeros (e.g. 1.00) for stringValue
-    late var unit ; //We get all ther others units
+    var length = Length(
+        removeTrailingZeros:
+            false); //initialize Length object, let's specify that we want to keep the trailing zeros (e.g. 1.00) for stringValue
+    late var unit; //We get all ther others units
     //var unit = length.kilometers; //We get all ther others units
     //print('name:${unit.name}, value:${unit.value}, stringValue:${unit.stringValue}, symbol:${unit.symbol}');
     print('type: ${type}');
@@ -31,8 +36,8 @@ class DistancePage extends StatelessWidget {
       case "mètre":
         length.convert(LENGTH.meters, value); //We give 1 meter as input
         break;
-      case "decimètre" :
-        length.convert(LENGTH.meters, (value/10)); //We give 1 meter as input
+      case "decimètre":
+        length.convert(LENGTH.meters, (value / 10)); //We give 1 meter as input
         break;
       case "centimètre":
         length.convert(LENGTH.centimeters, value); //We give 1 meter as input
@@ -52,19 +57,18 @@ class DistancePage extends StatelessWidget {
       case "pouce":
         length.convert(LENGTH.inches, value); //We give 1 meter as input
         break;
-
-  }
+    }
 
     switch (type) {
       case "kilomètre":
-        unit =  length.kilometers; //We give 1 meter as input
+        unit = length.kilometers; //We give 1 meter as input
         break;
       case "mètre":
         unit = length.meters;
         break;
-      case "decimètre" :
+      case "decimètre":
         unit = (length.meters);
-        return (unit.value)*10;
+        return (unit.value) * 10;
         break;
       case "centimètre":
         unit = length.centimeters;
@@ -84,78 +88,94 @@ class DistancePage extends StatelessWidget {
       case "pouce":
         unit = length.inches;
         break;
-      default :
+      default:
         unit = length.meters;
     }
     return unit.value;
-
-}
-
-  void changeValue(String val){
-     valueTop = val;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final title = 'Distance';
+  String convertValue(String value, String inputUnit, String convertUnit) {
+    double convertedValue = 0;
+    if (value.isEmpty) {
+      value = "0";
+    }
+    double valueToConvert = double.parse(value);
+    var length = Length(removeTrailingZeros: false);
+    late var unit;
+    switch (inputUnit) {
+      case "kilomètre":
+        length.convert(
+            LENGTH.kilometers, valueToConvert); //We give 1 meter as input
+        break;
+      case "mètre":
+        length.convert(
+            LENGTH.meters, valueToConvert); //We give 1 meter as input
+        break;
+      case "decimètre":
+        length.convert(
+            LENGTH.meters, (valueToConvert / 10)); //We give 1 meter as input
+        break;
+      case "centimètre":
+        length.convert(
+            LENGTH.centimeters, valueToConvert); //We give 1 meter as input
+        break;
+      case "milimètre":
+        length.convert(
+            LENGTH.millimeters, valueToConvert); //We give 1 meter as input
+        break;
+      case "nanomètre":
+        length.convert(
+            LENGTH.nanometers, valueToConvert); //We give 1 meter as input
+        break;
+      case "yard":
+        length.convert(LENGTH.yards, valueToConvert); //We give 1 meter as input
+        break;
+      case "pied":
+        length.convert(LENGTH.feet, valueToConvert); //We give 1 meter as input
+        break;
+      case "pouce":
+        length.convert(
+            LENGTH.inches, valueToConvert); //We give 1 meter as input
+        break;
+    }
 
-
-    //printData(title);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Chiffre decimal :"),
-            InputHelper().setInputField(context, "0", decimalData, TextInputType.number),
-            MyStatefulWidget(),
-            MyStatefulWidgetDown(),
-            Text("Resultat "),
-            InputHelper().setInputField(context, "0", romanData, TextInputType.text),
-            ElevatedButton(onPressed: () {
-              double? mesure = selectMesure(decimalData.text,dropdownValue,dropdownValueBottom);
-              romanData.text = mesure!.toStringAsExponential(3);
-              print('coucou '+dropdownValue);
-            }, child: Text("Calculer"))
-          ],
-        ),
-      ),
-    );
+    switch (convertUnit) {
+      case "kilomètre":
+        unit = length.kilometers; //We give 1 meter as input
+        break;
+      case "mètre":
+        unit = length.meters;
+        break;
+      case "decimètre":
+        unit = (length.meters);
+        unit.value *= 10;
+        break;
+      case "centimètre":
+        unit = length.centimeters;
+        break;
+      case "milimètre":
+        unit = length.millimeters;
+        break;
+      case "nanomètre":
+        unit = length.nanometers;
+        break;
+      case "yard":
+        unit = length.yards;
+        break;
+      case "pied":
+        unit = length.feet;
+        break;
+      case "pouce":
+        unit = length.inches;
+        break;
+      default:
+        unit = length.meters;
+    }
+    print(unit.value);
+    return unit.value.toStringAsFixed(2);
   }
 
-}
-
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
-  /*_MyStatefulWidgetState(isDropDownTop){
-    isDropDownTop = this.isDropDownTop;
-}*/
-
-  /*bool get isDropDownTop {
-    return isDropDownTop;
-  }
-
-  void set isDropDownTop(bool isDropDownTop) {
-    isDropDownTop = isDropDownTop;
-  }*/
-
-
-  @override
-  Widget build(BuildContext context) {
-    //print(this.isDropDownTop);
-    //String isDropped = this.isDropDownTop ? dropdownValue : dropdownValueBottom;
-
+  createUpDropdown() {
     return DropdownButton<String>(
       value: dropdownValue,
       icon: const Icon(Icons.arrow_downward),
@@ -167,27 +187,23 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         color: Colors.deepPurpleAccent,
       ),
       onChanged: (String? newValue) {
-
         setState(() {
-          dropdownValue = newValue! ;
-         // this.isDropDownTop ? dropdownValue = newValue! : dropdownValueBottom = newValue!;
+          dropdownValue = newValue!;
+          // this.isDropDownTop ? dropdownValue = newValue! : dropdownValueBottom = newValue!;
         });
-        DistancePage distancePage = new DistancePage();
-        final double? mesure;
-        try {
-          String valueInputTop = distancePage.decimalData.text;
-          print("The result is $valueInputTop");
-          mesure = distancePage.selectMesure(valueInputTop,dropdownValue,dropdownValueBottom);
-          DistancePage().romanData.text = mesure!.toStringAsExponential(3);
-
-        } catch (e) {
-          print("The exception thrown is $e");
-          // EditText EtPotential does not contain a valid double
-        }
-
+        bottomDropDownOnChanged(secondInputController.text);
       },
-      items: <String>['kilomètre', 'mètre','decimètre','centimètre', 'milimètre','nanomètre', 'yard','pied','pouce']
-          .map<DropdownMenuItem<String>>((String value) {
+      items: <String>[
+        'kilomètre',
+        'mètre',
+        'decimètre',
+        'centimètre',
+        'milimètre',
+        'nanomètre',
+        'yard',
+        'pied',
+        'pouce'
+      ].map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value),
@@ -195,21 +211,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       }).toList(),
     );
   }
-}
 
-class MyStatefulWidgetDown extends StatefulWidget {
-  const MyStatefulWidgetDown({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetStateDown createState() => _MyStatefulWidgetStateDown();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetStateDown extends State<MyStatefulWidgetDown> {
-
-
-  @override
-  Widget build(BuildContext context) {
+  createBottomDropDown() {
     return DropdownButton<String>(
       value: dropdownValueBottom,
       icon: const Icon(Icons.arrow_downward),
@@ -221,16 +224,22 @@ class _MyStatefulWidgetStateDown extends State<MyStatefulWidgetDown> {
         color: Colors.deepPurpleAccent,
       ),
       onChanged: (String? newValue) {
-
         setState(() {
-          dropdownValueBottom = newValue! ;
+          dropdownValueBottom = newValue!;
         });
-        double? mesure = DistancePage().selectMesure(valueTop,dropdownValue,dropdownValueBottom);
-        DistancePage().romanData.text = mesure!.toStringAsExponential(3);
-        print('la value  ${DistancePage().romanData.text} ');
+        upDropDownOnChanged(secondInputController.text);
       },
-      items: <String>['kilomètre', 'mètre','decimètre','centimètre', 'milimètre','nanomètre', 'yard','pied','pouce']
-          .map<DropdownMenuItem<String>>((String value) {
+      items: <String>[
+        'kilomètre',
+        'mètre',
+        'decimètre',
+        'centimètre',
+        'milimètre',
+        'nanomètre',
+        'yard',
+        'pied',
+        'pouce'
+      ].map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value),
@@ -238,5 +247,52 @@ class _MyStatefulWidgetStateDown extends State<MyStatefulWidgetDown> {
       }).toList(),
     );
   }
-}
 
+  void upDropDownOnChanged(String val) {
+    setState(() {
+      secondInputController.text =
+          convertValue(val, dropdownValue, dropdownValueBottom);
+    });
+  }
+
+  void bottomDropDownOnChanged(String val) {
+    setState(() {
+      firstInputController.text =
+          convertValue(val, dropdownValueBottom, dropdownValue);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Distance';
+
+    //printData(title);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Chiffre decimal :"),
+            InputHelper().setInputField(context, "0", firstInputController,
+                TextInputType.number, false, upDropDownOnChanged),
+            createUpDropdown(),
+            createBottomDropDown(),
+            Text("Resultat "),
+            InputHelper().setInputField(context, "0", secondInputController,
+                TextInputType.text, false, bottomDropDownOnChanged),
+            /*ElevatedButton(
+                onPressed: () {
+                  double? mesure = selectMesure(
+                      firstInputController.text, dropdownValue, dropdownValueBottom);
+                  secondInputController.text = mesure!.toStringAsExponential(3);
+                  print('coucou ' + dropdownValue);
+                },
+                child: Text("Calculer"))*/
+          ],
+        ),
+      ),
+    );
+  }
+}
