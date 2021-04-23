a81a55d487e22d88d09ddd22d1faae3c9b378d53
Thibaud <Thibaud.roussel@edu.itescia.fr>
2021-04-23 13:27:26 +0200

    date de dnaissance temps

--- flutter_app/lib/Controllers/DateDiffAge.dart
+++ flutter_app/lib/Controllers/DateDiffAge.dart
@@ -20,12 +20,12 @@ class _DateDiffPageState extends State<DateDiffAge> {
     String year = ((secondDate.year - firstDate.year).abs()).toString();
     String month = ((secondDate.month - firstDate.month).abs()).toString();
     String day = ((secondDate.day - firstDate.day).abs()).toString();
-    return "$year années, $month mois et $day jours";
+    return "$year années, $month mois et $day jours ";
   }
 
   _getNextBirthDay(String date1){
     DateTime firstDate = DateTime.parse(date1);
-
+    print('la date '+firstDate.toString());
     DateTime now = DateTime.now();
     String nextMonth;
     String nextDay;
@@ -36,25 +36,25 @@ class _DateDiffPageState extends State<DateDiffAge> {
 
 
 
-    print("date" + (year + month + day));
-    now = new DateTime(now.year + 1, now.month, now.day);
-    print("nextYear : " + year);
+   // print("date" + (year + month + day));
+    now = new DateTime(now.year, now.month, now.day);
+   // print("nextYear : " + year);
     nextMonth = now.month.toString();
     nextDay = now.day.toString();
 
-    DateTime nextYear = new DateTime(DateTime.now().year + 1,DateTime.now().month,DateTime.now().day);
+    DateTime nextYear = new DateTime(DateTime.now().year,DateTime.now().month,DateTime.now().day);
     int nextBdMonth = 0;
     int nextBdDay = 0;
+
+
+   int  result = nextYear.difference(firstDate).inDays;
 
    // DateTime calcul = int.parse(nextYear) - firstDate;
 
     //nextBdMonth = nextYear.month - firstDate.month;
     //nextBdDay = nextYear.day - firstDate.day;
     
-    nextYear.subtract(new Duration(days: ))
-    return "ton prochain anniversaire seras dans "
-        + (int.parse(nextMonth) - int.parse(month)).toString() + " mois, "
-        + (int.parse(nextDay) - int.parse(day)).toString() + " jours";
+    return "tu es né il y a  "+ result.toString()+" jours ";
   }
 
   @override
@@ -82,7 +82,7 @@ class _DateDiffPageState extends State<DateDiffAge> {
                 if  (_firstDate != "")
                 {
                   displayText = "il s'est passée " + _getDateDiff(_firstDate.toString()) + "depuis votre naissance";
-                  displayTextAge = "Ton prochain anniversaire est dans " + _getNextBirthDay(_firstDate.toString()) + "depuis votre naissance";
+                  displayTextAge = "Ton prochain anniversaire est dans " + _getNextBirthDay(_firstDate.toString()) + " depuis votre naissance";
                 }
               },
             ),
